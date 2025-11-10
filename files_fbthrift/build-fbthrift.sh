4a886d630bfab79b53be23c609037c9091b38ad9
Mark Shroyer <mark@shroyer.name>
2025-11-10 09:01:50 -0800

    Remove sources after building fbthrift

--- files_fbthrift/build-fbthrift.sh
+++ files_fbthrift/build-fbthrift.sh
@@ -18,6 +18,10 @@ patch -p1 </patches/fbthrift000.patch
 
 ./build/fbcode_builder/getdeps.py --allow-system-packages build fbthrift
 
+# Don't cache the source in the docker image, saving about half a GB.
+cd /
+rm -rf /fbthrift
+
 # I couldn't figure out how to change the output directory using getdeps, so
 # just clean up caches from the tmp dir instead.
 rm -rf /tmp/fbcode_builder_getdeps-ZfbthriftZbuildZfbcode_builder-root/{build,downloads,extracted,repos}
