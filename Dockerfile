899936222bcbb8631e441d0ddc5fe25a3e0c8f86
Mark Shroyer <mark@shroyer.name>
2025-11-12 12:16:11 -0800

    Build fbthrift archive

--- Dockerfile
+++ Dockerfile
@@ -20,18 +20,17 @@ RUN dnf install -y 'dnf-command(config-manager)' && \
 ARG files_fbthrift_cachebust=1
 COPY files_fbthrift/ /
 ARG fbthrift_commit=main
-RUN /build-fbthrift.sh ${fbthrift_commit}
-ENV PATH="${PATH}:/opt/fbthrift/bin"
+# RUN /build-fbthrift.sh ${fbthrift_commit}
 
-# Rust
-RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh && \
-    sh /tmp/rustup.sh --profile minimal -y && \
-    rm /tmp/rustup.sh
-ENV PATH="${PATH}:${HOME}/.cargo/bin"
+# # Rust
+# RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh && \
+#     sh /tmp/rustup.sh --profile minimal -y && \
+#     rm /tmp/rustup.sh
+# ENV PATH="${PATH}:${HOME}/.cargo/bin"
 
-# Keep Sapling patches and scripts separate so we can modify them without
-# losing the cached, time-consuming fbthrift build.
-ARG files_sapling_cachebust=2
-COPY files_sapling/ /
+# # Keep Sapling patches and scripts separate so we can modify them without
+# # losing the cached, time-consuming fbthrift build.
+# ARG files_sapling_cachebust=2
+# COPY files_sapling/ /
 
-ENTRYPOINT ["/build-sapling.sh"]
+# ENTRYPOINT ["/build-sapling.sh"]
--- files_fbthrift/build-fbthrift.sh
+++ files_fbthrift/build-fbthrift.sh
@@ -9,20 +9,51 @@ if [ -z "$commit" ]; then
 fi
 
 cd /
-git clone https://github.com/facebook/fbthrift.git
+if [ ! -d fbthrift ]; then
+	git clone https://github.com/facebook/fbthrift.git
+fi
 cd fbthrift
-git checkout "$commit"
 
+# In case we have an existing and already-patched clone.
+git reset --hard HEAD
+
+git checkout "$commit"
+HASH="$(git rev-parse HEAD | head -c8)"
 for patch in /patches/fbthrift*.patch; do
 	patch -p1 <"$patch"
 done
 
-./build/fbcode_builder/getdeps.py --allow-system-packages build fbthrift
+PREFIX="/opt/fbthrift"
+FBCODE_BUILDER_ROOT="/tmp/fbcode_builder_getdeps-ZfbthriftZbuildZfbcode_builder-root"
+INSTALLED="$FBCODE_BUILDER_ROOT/installed"
+THRIFT1="$INSTALLED/fbthrift${PREFIX}/bin/thrift1"
 
-# Don't cache the source in the docker image, saving about half a GB.
-cd /
-rm -rf /fbthrift
+if [ ! -f "$THRIFT1" ]; then
+	./build/fbcode_builder/getdeps.py \
+		--allow-system-packages build \
+		--src-dir=. fbthrift \
+		--project-install-prefix "fbthrift:$PREFIX"
+fi
 
-# I couldn't figure out how to change the output directory using getdeps, so
-# just clean up caches from the tmp dir instead.
-rm -rf /tmp/fbcode_builder_getdeps-ZfbthriftZbuildZfbcode_builder-root/{build,downloads,extracted,repos}
+# Running getdeps.py fixup-dyn-deps produces a binary with a missing "version"
+# symbol, so let's just copy and strip all the libraries ourselves.
+mkdir -p "$PREFIX/bin"
+cp "$THRIFT1" "$PREFIX/bin/"
+mkdir -p "$PREFIX/lib"
+find "$INSTALLED" -name '*.so' | xargs -I{} cp -a {} "$PREFIX/lib/"
+find "$INSTALLED" -name '*.so.*' | xargs -I{} cp -a {} "$PREFIX/lib/"
+find "$PREFIX" -type f | xargs -I{} strip {}
+
+cp -ar "$INSTALLED/fbthrift${PREFIX}/include" "$PREFIX/include"
+
+if [ -d /artifacts ]; then
+	tar -cJf "/artifacts/fbthrift-$(date +%Y%m%d.%H%M%S).${HASH}.tar.xz" "$PREFIX"
+fi
+
+# # Don't cache the source in the docker image, saving about half a GB.
+# cd /
+# rm -rf /fbthrift
+
+# # I couldn't figure out how to change the output directory using getdeps, so
+# # just clean up caches from the tmp dir instead.
+# rm -rf "$FBCODE_BUILDER"
