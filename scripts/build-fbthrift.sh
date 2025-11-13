a394f52ff590476713309eebc7c9e274b588c37a
Mark Shroyer <mark@shroyer.name>
2025-11-13 14:57:56 -0800

    Remove explicit chcon step
    Summary:
    Tries to forego an explicit call to chcon on ./artifacts as a prerequisite,
    instead relying on the :z flag.  Maybe docker supports this the same as podman
    does?
    
    Test Plan:

--- scripts/build-fbthrift.sh
+++ scripts/build-fbthrift.sh
@@ -7,10 +7,10 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 
 BUILDER_IMAGE_ID="$(mktemp)"
 "$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f fbthrift-builder/Dockerfile
-"$DOCKER" run -v ./artifacts:/artifacts "$(cat "$BUILDER_IMAGE_ID")"
+"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")"
 rm -f "$BUILDER_IMAGE_ID"
 
 TESTER_IMAGE_ID="$(mktemp)"
 "$DOCKER" build --iidfile="$TESTER_IMAGE_ID" -f fbthrift-tester/Dockerfile
-"$DOCKER" run -v ./artifacts:/artifacts:ro "$(cat "$TESTER_IMAGE_ID")"
+"$DOCKER" run -v ./artifacts:/artifacts:ro,z "$(cat "$TESTER_IMAGE_ID")"
 rm -f "$TESTER_IMAGE_ID"
