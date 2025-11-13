f439e7350f89d68afb51c5f66fe98f5843c85c3c
Mark Shroyer <mark@shroyer.name>
2025-11-13 10:50:41 -0800

    Add initial build-sapling.sh script
    Summary:
    Adds the first part of a Sapling build script.  Also fixes the path to the
    thrift1 binary in its wrapper script.
    
    Test Plan:

---
+++ scripts/build-sapling.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+SCRIPTS=$(cd "$(dirname "$0")" && pwd)
+. "$SCRIPTS/lib"
+
+BUILDER_IMAGE_ID="$(mktemp)"
+"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f sapling-builder/Dockerfile -v "$PWD/artifacts:/artifacts:ro" -t sapling-builder
+"$DOCKER" run -v ./artifacts:/artifacts "$(cat "$BUILDER_IMAGE_ID")"
+rm -f "$BUILDER_IMAGE_ID"
