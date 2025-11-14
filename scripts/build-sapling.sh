289b88d49829826b1d65afab00e24db9e9054883
Mark Shroyer <mark@shroyer.name>
2025-11-13 16:29:14 -0800

    Use :z option for sapling builder volume
    Summary:
    
    Test Plan:

--- scripts/build-sapling.sh
+++ scripts/build-sapling.sh
@@ -6,6 +6,6 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
 
 BUILDER_IMAGE_ID="$(mktemp)"
-"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f sapling-builder/Dockerfile -v "$PWD/artifacts:/artifacts:ro" -t sapling-builder
-"$DOCKER" run -v ./artifacts:/artifacts "$(cat "$BUILDER_IMAGE_ID")"
+"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f sapling-builder/Dockerfile -v "$PWD/artifacts:/artifacts:ro,z" -t sapling-builder
+"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")"
 rm -f "$BUILDER_IMAGE_ID"
