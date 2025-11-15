a331024834ee3aac7745a9fb8147e674215cec2e
Mark Shroyer <mark@shroyer.name>
2025-11-14 16:56:32 -0800

    [scripts] Commit hash arg for build-fbthrift.sh
    Summary:
    Adds an optional argument to build-fbthrift.sh giving a specific commit hash to
    build, which in turn is passed to the builder container.  Defaults to main if
    unspecified.
    
    Test Plan:
    Manual build

--- scripts/build-fbthrift.sh
+++ scripts/build-fbthrift.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+commit="$1"
+if [ -z "$commit" ]; then
+	commit="main"
+fi
+
 SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
 
 BUILDER_IMAGE_ID="$(mktemp)"
 "$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f fbthrift-builder/Dockerfile
-"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")"
+"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")" /run.sh "$commit"
 rm -f "$BUILDER_IMAGE_ID"
 
 TESTER_IMAGE_ID="$(mktemp)"
