bacec99211d1855538a11350f1fdc49e534a6950
Mark Shroyer <mark@shroyer.name>
2025-11-14 18:26:31 -0800

    Add sapling-tester image
    Summary:
    Adds a container for testing a sapling RPM on a fresh, bare AlmaLinux 10 image.
    This ensures the binary is linked correctly and that commands requiring
    external tools like git and nodejs work.
    
    Test Plan:
    Manually build and run image

---
+++ sapling-tester/files/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+RPM="$(printf '%s\n' /artifacts/x86_64/sapling-*.el10.x86_64.rpm | sort | tail -n1)"
+if [ ! -f "$RPM" ]; then
+	echo "No sapling build found in artifacts/!" >&2
+	exit 1
+fi
+
+echo "Checking sapling build: ${RPM}"
+
+try_sl() {
+	echo ""
+	echo "Trying \`sl $@\`..."
+	sl $@
+	echo "Success!"
+}
+
+dnf install -y "$RPM"
+try_sl --version
+try_sl clone https://github.com/mshroyer/sapling-builds
+cd sapling-builds
+try_sl web
+try_sl web --kill
