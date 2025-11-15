8d3d72260afad33410d08339a98dd42433fe9a04
Mark Shroyer <mark@shroyer.name>
2025-11-15 08:42:07 -0800

    [scripts] Accept commit argument in build-sapling.sh
    Summary:
    
    Test Plan:

--- scripts/build-sapling.sh
+++ scripts/build-sapling.sh
@@ -2,9 +2,16 @@
 
 # Build a fresh Sapling RPM for x86_64 AlmaLinux 10.
 #
-# Requires an existing build of fbthrift as a prerequisite.
+# Builds from main in https://github.com/facebook/sapling, or from a specific
+# commmit if one as given as an optional argument.  Requires an existing build
+# of fbthrift in ./artifacts as a prerequisite.
 
 set -e
+
+commit="$1"
+if [ -z "$commit" ]; then
+	commit="main"
+fi
 
 SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
@@ -25,5 +32,5 @@ ln "$LATEST_FBTHRIFT" sapling-builder/files/fbthrift.tar.xz
 # Build and run the sapling-builder container.
 IMAGE_ID="$(mktemp)"
 "$DOCKER" build --iidfile="$IMAGE_ID" ./sapling-builder
-"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$IMAGE_ID")"
+"$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$IMAGE_ID")" /run.sh "$commit"
 rm -f "$IMAGE_ID"
