ca3dce8bbc22aefdc4f3a9c4481ba3fcfa872c1b
Mark Shroyer <mark@shroyer.name>
2025-11-14 16:59:54 -0800

    Always provide context path when invoking docker/podman
    I think this should fix the build scripts so they work on docker as well as
    with podman.

--- scripts/build-fbthrift.sh
+++ scripts/build-fbthrift.sh
@@ -11,11 +11,11 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
 
 BUILDER_IMAGE_ID="$(mktemp)"
-"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f fbthrift-builder/Dockerfile
+"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" ./fbthrift-builder
 "$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")" /run.sh "$commit"
 rm -f "$BUILDER_IMAGE_ID"
 
 TESTER_IMAGE_ID="$(mktemp)"
-"$DOCKER" build --iidfile="$TESTER_IMAGE_ID" -f fbthrift-tester/Dockerfile
+"$DOCKER" build --iidfile="$TESTER_IMAGE_ID" ./fbthrift-tester
 "$DOCKER" run -v ./artifacts:/artifacts:z,ro "$(cat "$TESTER_IMAGE_ID")"
 rm -f "$TESTER_IMAGE_ID"
--- scripts/build-sapling.sh
+++ scripts/build-sapling.sh
@@ -6,6 +6,6 @@ SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
 
 BUILDER_IMAGE_ID="$(mktemp)"
-"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -f sapling-builder/Dockerfile -v "$PWD/artifacts:/artifacts:ro,z"
+"$DOCKER" build --iidfile="$BUILDER_IMAGE_ID" -v "$PWD/artifacts:/artifacts:ro,z" ./sapling-builder
 "$DOCKER" run -v ./artifacts:/artifacts:z "$(cat "$BUILDER_IMAGE_ID")"
 rm -f "$BUILDER_IMAGE_ID"
