3ab9a54f1e14fd50a84e22c0750d61e67ea3e098
Mark Shroyer <mark@shroyer.name>
2026-08-22 03:51:04 +0000

    Parameterize fbthrift-smoketest.sh distro

--- scripts/fbthrift-smoketest.sh
+++ scripts/fbthrift-smoketest.sh
@@ -2,13 +2,31 @@
 
 set -e
 
+distro=el10
+while getopts d: flag
+do
+	case "$flag" in
+		d)
+			distro="$OPTARG"
+			;;
+
+		*)
+			echo "Unknown flag: $flag" >&2
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+
 SCRIPTS=$(cd "$(dirname "$0")" && pwd)
 . "$SCRIPTS/lib"
 
 
+mirror_distro_files "./fbthrift-smoketester/$distro"
+
 FILES_CACHEBUST="$(latest_mtime_recursive ./fbthrift-smoketester/files)"
 IMAGE_ID="$(mktemp)"
-"$DOCKER" build --iidfile="$IMAGE_ID" ./fbthrift-smoketester \
+"$DOCKER" build --iidfile="$IMAGE_ID" "./fbthrift-smoketester/$distro" \
 	  --build-arg=files_cachebust=$FILES_CACHEBUST
 "$DOCKER" run -v ./artifacts:/artifacts:z,ro --rm "$(cat "$IMAGE_ID")"
 rm -f "$IMAGE_ID"
